8f92b8ee8dc548300cfd7104a876c30893458d31
Loris M <80474693+Loris-Moreau@users.noreply.github.com>
2023-05-26 17:29:14 +0200

    [fix] virtual mouse

--- Assets/Scripts/UI/Virtual Mouse/GamepadCursor.cs
+++ Assets/Scripts/UI/Virtual Mouse/GamepadCursor.cs
@@ -18,16 +18,34 @@ public class GamepadCursor : MonoBehaviour
 
     [SerializeField]
     private float cursorSpeed = 1000f;
+    [SerializeField]
+    private float padding = 35f;
+
+    private Mouse virtualMouse;
+    private Mouse currentMouse;
+    private Camera mainCamera;
 
     private bool previousMouseState;
 
-    private Mouse virtualMouse;
+    private string previousControlSceme = "";
+    private const string gamepadScheme = "Gamepad";
+    private const string mouseScheme = "Keyboard&Mouse";
 
-    private Camera mainCamera;
+    private void Update()
+    {
+        if (previousControlSceme != playerInput.currentControlScheme)
+        {
+            OnControlsChanged(playerInput);
+        }
+        previousControlSceme = playerInput.currentControlScheme;
+    }
 
-   private void OnEnable()
+    private void OnEnable()
    {
         mainCamera = Camera.main;
+        currentMouse = Mouse.current;
+
+        InputDevice virtualMouseInputDevice = InputSystem.GetDevice("VirtualMouse");
 
         if(virtualMouse == null)
         {
@@ -48,11 +66,16 @@ public class GamepadCursor : MonoBehaviour
         }
 
         InputSystem.onAfterUpdate += UpdateMotion;
-   }
+
+        playerInput.onControlsChanged += OnControlsChanged;
+    }
 
     private void OnDisable()
     {
+        if(virtualMouse != null && virtualMouse.added) InputSystem.RemoveDevice(virtualMouse);
+
         InputSystem.onAfterUpdate -= UpdateMotion;
+        playerInput.onControlsChanged -= OnControlsChanged;
     }
 
     private void UpdateMotion()
@@ -68,8 +91,8 @@ public class GamepadCursor : MonoBehaviour
         Vector2 currentPosition = virtualMouse.position.ReadValue();
         Vector2 newPosition = currentPosition + deltaValue;
 
-        newPosition.x = Mathf.Clamp(newPosition.x, 0, Screen.width);
-        newPosition.y = Mathf.Clamp(newPosition.y, 0, Screen.height);
+        newPosition.x = Mathf.Clamp(newPosition.x, padding, Screen.width - padding);
+        newPosition.y = Mathf.Clamp(newPosition.y, padding, Screen.height - padding);
 
         InputState.Change(virtualMouse.position, newPosition);
         InputState.Change(virtualMouse.delta, deltaValue);
@@ -93,5 +116,26 @@ public class GamepadCursor : MonoBehaviour
             canvas.renderMode == RenderMode.ScreenSpaceOverlay ? null : mainCamera, out anchoredPosition);
 
         cursorTransform.anchoredPosition = anchoredPosition;
+    }
+
+    private void OnControlsChanged(PlayerInput input)
+    {
+        if (playerInput.currentControlScheme == mouseScheme && previousControlSceme != mouseScheme)
+        {
+             cursorTransform.gameObject.SetActive(false);
+            Cursor.visible = true;
+            currentMouse.WarpCursorPosition(virtualMouse.position.ReadValue());
+
+            previousControlSceme = mouseScheme;
+        }
+        else if (playerInput.currentControlScheme == gamepadScheme && previousControlSceme != gamepadScheme)
+        {
+            cursorTransform.gameObject.SetActive(true);
+            Cursor.visible = false;
+
+            InputState.Change(virtualMouse.position, currentMouse.position.ReadValue());
+            AnchorCursor(currentMouse.position.ReadValue());
+            previousControlSceme = gamepadScheme;
+        }
     }
 }
