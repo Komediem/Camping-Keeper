711e578858414756d6a9f3afa6b0bda3eaa7e581
Komediem <gbussola@artfx.fr>
2023-06-02 10:59:07 +0200

    [Update] UI Tutos script

--- Assets/Scripts/UITutos.cs
+++ Assets/Scripts/UITutos.cs
@@ -6,5 +6,40 @@ using UnityEngine;
 public class UITutos : MonoBehaviour
 {
     [Header("Texts")]
-    [SerializeField] private TextMeshProUGUI tutoText; 
+    [SerializeField] private TextMeshProUGUI tutoText;
+    [SerializeField] private GameObject tutoTextObject;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("TutoJump"))
+        {
+            tutoText.text = "Press " + "E" + " to Jump";
+            StartCoroutine(tutoTextSpawn());
+        }
+
+        if(other.CompareTag("TutoPushPull"))
+        {
+            tutoText.text = "Hold " + "Z" + " to Push";
+            StartCoroutine(tutoTextSpawn());
+        }
+
+        if(other.CompareTag("TutoCrouch"))
+        {
+            tutoText.text = "Press " + "C" + " to Crouch";
+            StartCoroutine(tutoTextSpawn());
+        }
+
+        if(other.CompareTag("TutoInteract"))
+        {
+            tutoText.text = "Press " + "E" + " to Interact";
+            StartCoroutine(tutoTextSpawn());
+        }
+    }
+
+    IEnumerator tutoTextSpawn()
+    {
+        tutoTextObject.SetActive(true);
+        yield return new WaitForSeconds(3);
+        tutoTextObject.SetActive(false);
+    }
 }
