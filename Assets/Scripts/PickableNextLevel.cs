5c448565b64bcbe438a465fd8e9ecd98457bb47a
Komediem <gbussola@artfx.fr>
2023-06-05 11:10:34 +0200

    [Update] Pickable Work 1

--- Assets/Scripts/PickableNextLevel.cs
+++ Assets/Scripts/PickableNextLevel.cs
@@ -22,7 +22,7 @@ public class PickableNextLevel : MonoBehaviour
 
             model.SetActive(false);
 
-            //animator.SetBool("IsPicking", true);
+            animator.SetBool("IsPicking", true);
             //add more option when you pick up
             Invoke("NextLevel", TempsPickupAnimation);
         }
