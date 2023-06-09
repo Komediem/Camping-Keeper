9c61dd3d5d80fe3816fadb24a737341a069cb3d6
Komediem <gbussola@artfx.fr>
2023-06-09 11:58:07 +0200

    [Update] Tuto Texts

--- Assets/Scripts/UITutos.cs
+++ Assets/Scripts/UITutos.cs
@@ -6,37 +6,35 @@ using UnityEngine;
 public class UITutos : MonoBehaviour
 {
     [Header("Texts")]
-    [SerializeField] private TextMeshProUGUI tutoText;
     [SerializeField] private GameObject tutoTextObject;
 
-    [SerializeField] private Sprite[] tutoImages;
+    private void Start()
+    {
+        
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if(other.CompareTag("TutoJump"))
         {
-            tutoText.text = "Press " + "Space Bar" + " to Jump";
             StartCoroutine(tutoTextSpawn());
             other.GetComponent<BoxCollider>().enabled = false;
         }
 
         if(other.CompareTag("TutoPushPull"))
         {
-            tutoText.text = "Hold " + "Z" + " to Push or Pull";
             StartCoroutine(tutoTextSpawn());
             other.GetComponent<BoxCollider>().enabled = false;
         }
 
         if(other.CompareTag("TutoCrouch"))
         {
-            tutoText.text = "Press " + "C" + " to Crouch";
             StartCoroutine(tutoTextSpawn());
             other.GetComponent<BoxCollider>().enabled = false;
         }
 
         if(other.CompareTag("TutoInteract"))
         {
-            tutoText.text = "Press " + "E" + " to Interact";
             StartCoroutine(tutoTextSpawn());
             other.GetComponent<BoxCollider>().enabled = false;
         }
