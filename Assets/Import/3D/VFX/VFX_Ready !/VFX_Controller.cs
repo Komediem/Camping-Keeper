a1ca6c364dfca87560080a84158b0e3d3867906d
KamiPedro <kamipedro123@gmail.com>
2023-06-12 00:07:02 +0200

    VFX-Scripts

---
+++ Assets/Import/3D/VFX/VFX_Ready !/VFX_Controller.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.VFX;
+
+
+[RequireComponent(typeof(VisualEffect))]
+public class VFX_Controller : VFX_Manager
+{
+/*    private playerObject Player;
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            if (visualEffect != null) visualEffect.Play();
+        }
+
+
+
+    }
+
+
+
+    void Update()
+    {
+
+    }*/
+}
---
+++ Assets/Import/3D/VFX/VFX_Ready !/VFX_Manager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VFX;
+
+public class VFX_Manager : MonoBehaviour
+{
+    public VisualEffect Dust;
+    public VisualEffect AuraSpline;
+    public VisualEffect FireFlies;
+    public VisualEffect Sparks;
+    public VisualEffect Sparkle;
+    public VisualEffect Enemy;
+
+    private void Start()
+    {
+        
+    }
+}
