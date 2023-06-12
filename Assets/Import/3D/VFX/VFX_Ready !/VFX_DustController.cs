905a75c5853d874ffefd24fcd57b6dd132151b67
KamiPedro <kamipedro123@gmail.com>
2023-06-12 11:43:18 +0200

    VFX - SimpleDustController

--- Assets/Import/3D/VFX/VFX_Ready !/VFX_DustController.cs
+++ Assets/Import/3D/VFX/VFX_Ready !/VFX_DustController.cs
@@ -2,13 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class VFX_Dust : MonoBehaviour
+public class VFX_Dust : VFX_Manager
 {
     private void OnTriggerEnter(Collider collision)
     {
         if (collision.CompareTag("Player"))
         {
-            
+            if (Dust != null) Dust.Play();
         }
     }
 }
