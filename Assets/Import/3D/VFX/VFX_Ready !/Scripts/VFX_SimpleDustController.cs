1a343703849e13c93c2a83b74dec5b1d385c7172
KamiPedro <kamipedro123@gmail.com>
2023-06-12 18:11:34 +0200

    VFX - JumpDust Prefabs

--- Assets/Import/3D/VFX/VFX_Ready !/Scripts/VFX_SimpleDustController.cs
+++ Assets/Import/3D/VFX/VFX_Ready !/Scripts/VFX_SimpleDustController.cs
@@ -7,7 +7,10 @@ public class VFX_SimpleDustController : MonoBehaviour
 {
     [SerializeField] private VisualEffect dustVFX;
 
-
+    private void Start()
+    {
+        dustVFX = GetComponent<VisualEffect>();
+    }
     private void OnTriggerEnter(Collider collision)
     {
         if (collision.CompareTag("Player"))
