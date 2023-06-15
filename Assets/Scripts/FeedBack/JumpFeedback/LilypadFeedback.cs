484ca8c37766f490ceda1b8703ef4efa5f55be23
ThAb <tabate@artfx.fr>
2023-06-15 15:06:17 +0200

    [add] feedback

--- Assets/Scripts/FeedBack/JumpFeedback/LilypadFeedback.cs
+++ Assets/Scripts/FeedBack/JumpFeedback/LilypadFeedback.cs
@@ -6,9 +6,12 @@ using UnityEngine.VFX;
 public class LilypadFeedback : JumpFeedbackClass
 {
     public ParticleSystem splash;
+    [SerializeField] AudioClip[] audioClip;
+    private AudioSource audioSource;
 
     private void Awake()
     {
+        audioSource = GetComponent<AudioSource>();
         feedbackJumpAnimator = GetComponent<Animator>();
     }
 
@@ -16,5 +19,20 @@ public class LilypadFeedback : JumpFeedbackClass
     {
         splash.Play();
         feedbackJumpAnimator.SetTrigger("PloufTrigger");
+        _Bowing();
+    }
+
+    public void _Bowing()
+    {
+        AudioClip clip = GetRandomClip();
+        audioSource.PlayOneShot(clip);
+    }
+
+    private AudioClip GetRandomClip()
+    {
+        int index = Random.Range(0, audioClip.Length - 1);
+        audioSource.volume = Random.Range(0.2f, 0.4f);
+        audioSource.pitch = Random.Range(0.95f, 1.05f);
+        return audioClip[index];
     }
 }
--- Assets/Scripts/FeedBack/JumpFeedback/TrampolineFeedbacks.cs
+++ Assets/Scripts/FeedBack/JumpFeedback/TrampolineFeedbacks.cs
@@ -4,13 +4,33 @@ using UnityEngine;
 
 public class TrampolineFeedbacks : JumpFeedbackClass
 {
+    [SerializeField] AudioClip[] audioClip;
+    private AudioSource audioSource;
+
     private void Awake()
     {
         feedbackJumpAnimator = GetComponent<Animator>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     public override void ReturnFeedback()
     {
+        
         feedbackJumpAnimator.SetTrigger("isBoing");
+        _Bowing();
+    }
+
+    public void _Bowing()
+    {
+        AudioClip clip = GetRandomClip();
+        audioSource.PlayOneShot(clip);
+    }
+
+    private AudioClip GetRandomClip()
+    {
+        int index = Random.Range(0, audioClip.Length - 1);
+        audioSource.volume = Random.Range(0.2f, 0.4f);
+        audioSource.pitch = Random.Range(0.95f, 1.05f);
+        return audioClip[index];
     }
 }
