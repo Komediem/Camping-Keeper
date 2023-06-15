8d9922d80da5543c9dd76e2781f6d2a257057f08
KamiPedro <kamipedro123@gmail.com>
2023-06-15 14:41:32 +0200

    VFX - Platform Dust Works

--- Assets/Import/3D/VFX/VFX_Ready !/Scripts/VFX_SimpleDustController.cs
+++ Assets/Import/3D/VFX/VFX_Ready !/Scripts/VFX_SimpleDustController.cs
@@ -7,14 +7,19 @@ public class VFX_SimpleDustController : MonoBehaviour
 {
     [SerializeField] private VisualEffect dustVFX;
 
+    [SerializeField] BoxCollider boxCol;
+
     private void Start()
     {
+        boxCol = GetComponent<BoxCollider>();
         dustVFX = GetComponent<VisualEffect>();
     }
     private void OnTriggerEnter(Collider collision)
     {
+        Debug.Log("Platform Trigger");
         if (collision.CompareTag("Player"))
         {
+            Debug.Log("That's a player on the Platform !");
             if (dustVFX != null) dustVFX.Play();
         }
     }
