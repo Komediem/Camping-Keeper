387d6bcc610d108ef133692d52aacca90c4b76d9
ThAb <tabate@artfx.fr>
2023-06-15 10:49:18 +0200

    [add] sound for tranpo ans ghost

--- Assets/Scripts/FeedBack/JumpFeedback/TrampolineFeedbacks.cs
+++ Assets/Scripts/FeedBack/JumpFeedback/TrampolineFeedbacks.cs
@@ -2,15 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
 public class TrampolineFeedbacks : JumpFeedbackClass
 {
+    [SerializeField] AudioClip[] audioClip;
+    private AudioSource audioSource;
     private void Awake()
     {
+        audioSource = GetComponent<AudioSource>();
         feedbackJumpAnimator = GetComponent<Animator>();
     }
 
     public override void ReturnFeedback()
     {
         feedbackJumpAnimator.SetTrigger("isBoing");
+        _Bowing();
+    }
+
+    public void _Bowing()
+    {
+        AudioClip clip = GetRandomClip();
+        audioSource.PlayOneShot(clip);
+    }
+
+    private AudioClip GetRandomClip()
+    {
+        int index = Random.Range(0, audioClip.Length - 1);
+        audioSource.volume = Random.Range(0.2f, 0.4f);
+        audioSource.pitch = Random.Range(0.95f, 1.05f);
+        return audioClip[index];
     }
 }
