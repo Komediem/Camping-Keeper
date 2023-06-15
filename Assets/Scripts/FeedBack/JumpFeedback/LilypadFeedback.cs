14790325336e76007bae3042e3eb250b8019f170
ThAb <tabate@artfx.fr>
2023-06-15 11:57:44 +0200

    [add] feedback for lilypad

--- Assets/Scripts/FeedBack/JumpFeedback/LilypadFeedback.cs
+++ Assets/Scripts/FeedBack/JumpFeedback/LilypadFeedback.cs
@@ -6,15 +6,33 @@ using UnityEngine.VFX;
 public class LilypadFeedback : JumpFeedbackClass
 {
     public ParticleSystem splash;
+    [SerializeField] AudioClip[] audioClip;
+    private AudioSource audioSource;
 
     private void Awake()
     {
+        audioSource = GetComponent<AudioSource>();
         feedbackJumpAnimator = GetComponent<Animator>();
     }
 
     public override void ReturnFeedback()
     {
         splash.Play();
+        _Bowing();
         feedbackJumpAnimator.SetTrigger("PloufTrigger");
+    }
+
+    public void _Bowing()
+    {
+        AudioClip clip = GetRandomClip();
+        audioSource.PlayOneShot(clip);
+    }
+
+    private AudioClip GetRandomClip()
+    {
+        int index = Random.Range(0, audioClip.Length - 1);
+        audioSource.volume = Random.Range(0.2f, 0.4f);
+        audioSource.pitch = Random.Range(0.95f, 1.05f);
+        return audioClip[index];
     }
 }
